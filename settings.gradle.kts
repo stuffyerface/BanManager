a76b62f5c2ecf1f6609298b8663b0577c5da4c08
Stuffy <stuffinessmc@gmail.com>
2025-10-12 11:25:49 -0400

    ignore not velocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,9 @@
 rootProject.name = "BanManager"
 
 include(":BanManagerCommon")
-include(":BanManagerBukkit")
-include(":BanManagerBungee")
-include(":BanManagerSponge")
 include(":BanManagerLibs")
 include(":BanManagerVelocity")
-include(":BanManagerFabric")
 
 project(":BanManagerCommon").projectDir = file("common")
-project(":BanManagerBukkit").projectDir = file("bukkit")
-project(":BanManagerBungee").projectDir = file("bungee")
-project(":BanManagerSponge").projectDir = file("sponge")
 project(":BanManagerLibs").projectDir = file("libs")
 project(":BanManagerVelocity").projectDir = file("velocity")
-project(":BanManagerFabric").projectDir = file("fabric")
